5967b7ea0e45433045c513f9e7eafaa77cac3dfd
Marcus Lorenzana <lorenzanamarcus@gmail.com>
2015-04-27 20:36:22 -0500

    Significantly sped up the file processing
    Also cleaned up quite a bit

--- ClimateClean/src/climate/ClimateCleanStates.java
+++ ClimateClean/src/climate/ClimateCleanStates.java
@@ -5,7 +5,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This program will read in a large file containing many stations and states,
@@ -22,18 +26,10 @@ import java.util.Scanner;
 public class ClimateCleanStates {
 
 	public static void main(String[] args) throws IOException {
-		String[] states = { "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC",
-				"DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
-				"LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE",
-				"NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
-				"PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA",
-				"WV", "WI", "WY" };
 
 		Scanner in = new Scanner(System.in);
 		System.out
-				.println("Note: This program can take a long time depending on processor.");
-		System.out
-				.println("It is also not necessary to run as state files are in this directory.");
+				.println("It is not necessary to run as state files are in this directory.");
 		System.out
 				.println("But if you would like to see how it works, you may continue.");
 		System.out.println("Please remove state files before running.");
@@ -41,71 +37,68 @@ public class ClimateCleanStates {
 		String answer = in.nextLine();
 
 		if (answer.equals("N")) {
-			System.exit(0);
 			in.close();
+			System.exit(0);
+			
 		}
 		System.out.println("Would you like to run the program?");
 		String answer2 = in.nextLine();
 		if (answer2.equals("N")) {
-			System.exit(0);
 			in.close();
+			System.exit(0);
+			
 		}
 
-		String[] statesSpaced = new String[51];
 
-		File statefile, dir, infile;
+		File dir, infile;
 
 		// Create files for each states
 		dir = new File("States");
 		dir.mkdir();
-
-		// Modify array so there is a space between state abbreviations.
-		for (int i = 0; i < states.length; i++) {
-			String state = states[i];
-			String space_state = " " + state + " ";
-			statesSpaced[i] = space_state;
-			System.out.print(i + " : " + states[i] + "\n");
-		}
 
 		infile = new File("climatedata.csv");
 		FileReader fr = new FileReader(infile);
 		BufferedReader br = new BufferedReader(fr);
 
 		String line;
+		line = br.readLine();
 		System.out.println();
 
 		// Read in climatedata.csv
-		// Probably need to implement ClimateRecord class
+		final long start = System.currentTimeMillis();
+		Pattern p = Pattern.compile(".* ([A-Z][A-Z]) US");
+		Map<String, FileWriter> fileMap = new HashMap<String, FileWriter>();
+		String stateFileName="";
+
 		while ((line = br.readLine()) != null) {
-			// Remove instances of -9999
-			if (!line.contains("-9999")) {
-				for (int i = 0; i < states.length; i++) {
+		    if (!line.contains("-9999")) {
+		    	Matcher m = p.matcher(line);
+		        if (m.find()) {
+		            stateFileName = m.group(1); 
+		            stateFileName = "States/" + stateFileName + ".csv";
+		            FileWriter stateFileWriter = fileMap.get(stateFileName);
+		            if (stateFileWriter == null) {
+		                stateFileWriter = new FileWriter(stateFileName, true);
+		                fileMap.put(stateFileName, stateFileWriter);
+		            }
 
-					if (line.contains(statesSpaced[i])) {
-
-						// Create and select state file
-						String stateFileName = states[i];
-						stateFileName = "States/" + stateFileName + ".csv";
-						statefile = new File(stateFileName);
-
-						FileWriter stateWriter = new FileWriter(statefile, true);
-						stateWriter.write(line + "\n");
-						// Progress reporting
-						System.out.printf("Writing [%s] to file [%s]\n", line,
-								statefile);
-						stateWriter.flush();
-						stateWriter.close();
-
-					}
-
-				}
-
-			}
-
+		            stateFileWriter.write(line + "\n");
+		        }
+		    }
 		}
+
+		// flush the writers and close once you have parsed the entire file
+		for(Map.Entry<String, FileWriter> entry : fileMap.entrySet()) {
+		    FileWriter writer = entry.getValue();
+		    writer.flush();
+		    writer.close();
+		}
+		System.out.println("Elapsed " + (System.currentTimeMillis() - start)
+				+ " ms");
 		br.close();
 		fr.close();
 		in.close();
+
 
 	}
 
